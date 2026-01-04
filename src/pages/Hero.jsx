02e9cfd371f9bf353f2fb5b15e0343cdf432c30d
rajat-das247365 <rd540032@gmail.com>
2026-01-04 16:17:12 +0530

    updated code

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
 
 const Hero = () => {
-  return (
-    <div className='w-full min-h-screen border border-slate-800'>
-      
-    </div>
-  )
+    return (
+        <div className=' w-full h-[95%] border border-slate-800 flex '>
+            <div className=" left w-[70%] h-full  flex flex-col items-center justify-center px-10 gap-5 text-slate-500">
+                <h4 className='text-3xl font-bold tracking-widest'>HELLO<span className='text-indigo-500'>!</span></h4>
+                <h1 className='text-6xl font-bold'>I'm <span className='text-indigo-500 tracking-wider' >RAJAT KUMAR DAS</span></h1>
+                <h4 className='text-5xl font-extrabold text-slate-300 leading-relaxed tracking-wider'>Front End Developer</h4>
+                <h4 className='text-center text-xl'>Front end developer with hands on experience building real world web interfaces using <span className='text-indigo-500 font-semibold underline decoration-indigo-500 underline-offset-4' >React and modern CSS</span></h4>
+                <div className='btn  text-center leading-relaxed text-slate-200 rounded-2xl '>
+                    <button className='w-40 bg-[#032236] text-xl font-semibold p-1 hover:text-[] border border-[#63D0FF] text-[#63D0FF] mt-4 cursor-pointer'>Hire Me</button>
+                </div>
+                <div className="social flex w-50 justify-around p-3 text-2xl text-slate-200">
+                    <a href=""><FaGithub /></a>
+                    <a href=""><FaXTwitter /></a>
+                    <a href=""><FaLinkedin /></a>
+                </div>
+            </div>
+            <div className="right border w-[30%]"></div>
+        </div>
+    )
 }
 
 export default Hero
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,36 +7,37 @@ const Home = () => {
       <div className="border w-10 min-h-full border-t-0 border-l-0 border-gray-600/50  bg-[repeating-linear-gradient(135deg,rgba(255,255,255,0.04)_0,rgba(255,255,255,0.09)_1px,transparent_1px,transparent_8px)]"></div>
 
       <div className="w-[80%] flex flex-col items-center">
+        
         {/* Hero Nav */}
-        <div className="sticky top-15 w-[60%] h-10 border border-slate-800 mt-10 flex justify-around items-center bg-[repeating-linear-gradient(135deg,rgba(255,255,255,0.04)_0,rgba(255,255,255,0.09)_1px,transparent_1px,transparent_8px)] ">
+        <div className="sticky top-14 w-[60%] h-10 border border-slate-800 mt-10 flex justify-around items-center bg-[repeating-linear-gradient(135deg,rgba(255,255,255,0.04)_0,rgba(255,255,255,0.09)_1px,transparent_1px,transparent_8px)] ">
           <Link
-            className="text-[#63D0FF] font-semibold text-xl hover:text-slate-200"
+            className="text-[#63D0FF] font-semibold text-l hover:text-slate-200"
             to="hero"
           >
             HERO
           </Link>
           <Link
-            className="text-[#63D0FF] font-semibold text-xl hover:text-slate-200"
+            className="text-[#63D0FF] font-semibold text-l hover:text-slate-200"
             to="skills"
           >
             SKILLS
           </Link>
           <Link
-            className="text-[#63D0FF] font-semibold text-xl hover:text-slate-200"
+            className="text-[#63D0FF] font-semibold text-l hover:text-slate-200"
             to="featuredprojects"
           >
             FEATURED
           </Link>
           <Link
-            className="text-[#63D0FF] font-semibold text-xl hover:text-slate-200"
+            className="text-[#63D0FF] font-semibold text-l hover:text-slate-200"
             to="calltoaction"
           >
-            CALL TO ACTION
+            CTA
           </Link>
         </div>
 
         {/* Nested content */}
-        <div className="w-full mt-20">
+        <div className="w-full h-full mt-15">
           <Outlet />
         </div>
       </div>
