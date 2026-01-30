61ce5ba0b6f6eb0f791b76213000051fd2b47435
rajat-das247365 <rd540032@gmail.com>
2026-01-30 12:06:43 +0530

    s

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
                             rel="noreferrer"
              
                         >
-                            Downlaod CV
+                            Download CV
                         </a>
 
 
